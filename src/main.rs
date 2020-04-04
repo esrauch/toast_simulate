84becc77d384cd493505f4c620b980ca6a8b604a
Em Rauch <esrauch@gmail.com>
2020-04-04 15:58:55 -0400

    Working program that goes for max score

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,16 @@ fn d6() -> u32 {
 }
 
 #[derive(Debug)]
-struct TwoDice(u32, u32);
-
-impl TwoDice {
-    fn roll() -> TwoDice {
-        TwoDice(d6(), d6())
-    }
-}
-
-#[derive(Debug)]
 struct Card(u32);
 
 impl Card {
     fn new(value: u32) -> Card {
         assert!(value > 0 && value < 7);
         Card(value)
+    }
+
+    fn value(&self) -> u32 {
+        self.0
     }
 }
 
@@ -58,20 +53,43 @@ impl Deck {
     // A toast pulls 3 cards from the deck.
     fn toast(&mut self) -> Toast {
         assert!(self.cards.len() >= 3);
-        Toast(
-            self.cards.pop().unwrap(),
-            self.cards.pop().unwrap(),
-            self.cards.pop().unwrap(),
-        )
+        Toast {
+            cards: vec![
+                self.cards.pop().unwrap(),
+                self.cards.pop().unwrap(),
+                self.cards.pop().unwrap(),
+            ],
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.cards.is_empty()
     }
 }
 
 #[derive(Debug)]
-struct Toast(Card, Card, Card);
+struct Toast {
+    cards: Vec<Card>,
+}
 
 impl Toast {
-    fn to_values(&self) -> (u32, u32, u32) {
-        ((self.0).0, (self.1).0, (self.2).0)
+    fn to_values(&self) -> Vec<u32> {
+        self.cards.iter().map(|x| x.0).collect()
+    }
+
+    fn sum(&self) -> u32 {
+        self.to_values().iter().sum()
+    }
+    fn is_empty(&self) -> bool {
+        self.cards.is_empty()
+    }
+
+    fn try_clear_first(&mut self, r0: u32, r1: u32) {
+        // Given the two dice, if either matches the front then remove it.
+        let front = self.cards.first().unwrap().value();
+        if r0 == front || r1 == front {
+            self.cards.remove(0);
+        }
     }
 }
 
@@ -88,15 +106,51 @@ fn toast_pulls_three() {
     let initial_total_value = d.total_value();
     assert_eq!(d.cards.len(), 24);
     let t = d.toast();
-    let tvs = t.to_values();
-    assert!(tvs.0 > 0 && tvs.1 > 0 && tvs.2 > 0);
-    assert!(tvs.0 < 7 && tvs.1 < 7 && tvs.2 < 7);
+    assert_eq!(t.cards.len(), 3);
     assert_eq!(d.cards.len(), 21);
-    assert_eq!(d.total_value() + tvs.0 + tvs.1 + tvs.2, initial_total_value);
+    assert_eq!(d.total_value() + t.sum(), initial_total_value);
+}
+
+/// An attempt either wins and gives the score back,
+/// or it loses and gives a None back.
+fn attempt(deck: &mut Deck) -> Option<u32> {
+    // First you toast (Deal 3 cards)
+    let mut t = deck.toast();
+
+    // Number of rolls you get is the total.
+    let mut rolls = t.sum();
+
+    // It's also the score we get if we beat the toast
+    let score_if_win = t.sum();
+    while rolls > 0 {
+        rolls -= 1;
+        // You get two dice on a roll, use them to try to clear a card
+        t.try_clear_first(d6(), d6());
+        // See if we've beaten all 3 cards
+        if t.is_empty() {
+            return Some(score_if_win);
+        }
+    }
+    return None;
+}
+
+fn max_score_attempt() -> bool {
+    let mut deck = Deck::new();
+    loop {
+        if let None = attempt(&mut deck) {
+            return false;
+        }
+        if deck.is_empty() {
+            return true;
+        }
+    };
 }
 
 fn main() {
-    let mut deck = Deck::new();
-    let t = deck.toast();
-    println!("Hello, world! {:?}", t);
+    let attempts = 10000;
+    let wins = (0..attempts)
+        .map(|_| max_score_attempt())
+        .filter(|b| *b)
+        .count();
+    println!("{} of {} attempts won", wins, attempts);
 }
